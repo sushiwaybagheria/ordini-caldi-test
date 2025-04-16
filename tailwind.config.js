7553e466b47dfa8b7cf0953023e2f12bc0c3e997
sushiwaybagheria <sushiwaybagheria@gmail.com>
2025-04-16 14:11:35 +0200

    fix: configurazione Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
-
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
