ddc5675629e06ad76b5ecdf39a829d2869b48d9a
sushiwaybagheria <sushiwaybagheria@gmail.com>
2025-04-17 10:38:12 +0200

    fix: disabilitata cache proxy + forza dati aggiornati

--- api/proxy.js
+++ api/proxy.js
@@ -2,11 +2,15 @@
 
 export default async function handler(req, res) {
   const url = "https://script.google.com/macros/s/AKfycby0U00VFwJN2VeGQZiP0gLYZi5WNA5R3erjI4Nl4dePQ2RjLgqcDDh2hLZ-0ikteEI/exec";
-
+             
   try {
     const response = await fetch(url);
     const data = await response.json();
-    res.setHeader("Cache-Control", "s-maxage=30, stale-while-revalidate");
+
+
+res.setHeader("Cache-Control", "no-store");
+
+
     res.status(200).json(data);
   } catch (err) {
     console.error("Errore nella proxy API:", err);
