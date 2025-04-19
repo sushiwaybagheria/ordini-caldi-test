ecddbf5e7d9603840cc4f789f399fac08d9b6e0f
sushiwaybagheria <sushiwaybagheria@gmail.com>
2025-04-19 11:31:07 +0200

    feat: aggiunto firebase.js per salvataggio stato ordini

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCAnrXWrGm5bGpa-m-TdnHDn9baWLo_h7I",
+  authDomain: "ordini-caldi.firebaseapp.com",
+  projectId: "ordini-caldi",
+  storageBucket: "ordini-caldi.firebasestorage.app",
+  messagingSenderId: "930167832668",
+  appId: "1:930167832668:web:51a9928a5ac8047ae9a699"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
